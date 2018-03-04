c190dd4cd0ed571120502a1d620bb6d5fb92e82e
mrjonas <jonas.l.antanaitis@gmail.com>
2018-03-04 07:58:37 +0200

    webpack removed -p for testing

--- webpack.config.js
+++ webpack.config.js
@@ -2,100 +2,105 @@ const webpack = require("webpack");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const autoprefixer = require("autoprefixer");
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 
 const browserConfig = {
-  entry: "./src/browser/index.js",
-  output: {
-    path: './dist/',
-    filename: "bundle.js"
-  },
-  devtool: "cheap-module-source-map",
-  module: {
-    rules: [
-        {
-            test: /\.(eot|woff|woff2|ttf|svg|png|jpe?g|gif)(\?\S*)?$/,
-            loader: 'url-loader?limit=100000&name=./public/build/[hash].[ext]'
-        },
-      {
-        test: [/\.svg$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
-        loader: "file-loader",
-        options: {
-          name: "dist/media/[name].[ext]"
-        }
-      },
-      {
-        test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          use: [
+    entry: "./src/browser/index.js",
+    output: {
+        path: './dist/',
+        filename: "bundle.js"
+    },
+    devtool: "cheap-module-source-map",
+    module: {
+        rules: [
             {
-              loader: "css-loader",
-              options: { importLoaders: 1 }
+                test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader',
+                query: {
+                    presets: ['react']
+                }
+            },
+            {test: /\.jsx$/, exclude: /node_modules/, loaders: ['babel-loader']},
+            {
+                test: /\.(eot|woff|woff2|ttf|svg|png|jpe?g|gif)(\?\S*)?$/,
+                loader: 'url-loader?limit=100000&name=./public/build/[hash].[ext]'
             },
             {
-              loader: "postcss-loader",
-              options: { plugins: [autoprefixer()] }
+                test: [/\.svg$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+                loader: "file-loader",
+                options: {
+                    name: "dist/media/[name].[ext]"
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    use: [
+                        {
+                            loader: "css-loader",
+                            options: {importLoaders: 1}
+                        },
+                        {
+                            loader: "postcss-loader",
+                            options: {plugins: [autoprefixer()]}
+                        }
+                    ]
+                })
             }
-          ]
-        })
-      },
-      {
-        test: /js$/,
-        exclude: /(node_modules)/,
-        loader: "babel-loader",
-        query: { presets: ["react-app"] }
-      }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin({
+            filename: "css/[name].css"
+        }),
+        new CopyWebpackPlugin([
+            {from: 'src/assets'}
+        ])
     ]
-  },
-  plugins: [
-    new ExtractTextPlugin({
-      filename: "css/[name].css"
-    }),
-    new CopyWebpackPlugin([
-        {from: 'src/assets'}
-    ])
-  ]
 };
 
 const serverConfig = {
-  entry: "./src/server/index.js",
-  target: "node",
-  output: {
-    path: __dirname,
-    filename: "dist-server/server.js",
-    libraryTarget: "commonjs2"
-  },
-  devtool: "cheap-module-source-map",
-  module: {
-    rules: [
-      {
-        test: [/\.svg$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
-        loader: "file-loader",
-        options: {
-          name: "dist/media/[name].[ext]",
-          publicPath: url => url.replace(/dist/, ""),
-          emit: false
-        }
-      },
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: "css-loader/locals"
-          }
+    entry: "./src/server/index.js",
+    target: "node",
+    output: {
+        path: __dirname,
+        filename: "dist-server/server.js",
+        libraryTarget: "commonjs2"
+    },
+    devtool: "cheap-module-source-map",
+    module: {
+        rules: [
+            {
+                test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader',
+                query: {
+                    presets: ['react']
+                }
+            },
+            {test: /\.jsx$/, exclude: /node_modules/, loaders: ['babel-loader']},
+            {
+                test: [/\.svg$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+                loader: "file-loader",
+                options: {
+                    name: "dist/media/[name].[ext]",
+                    publicPath: url => url.replace(/dist/, ""),
+                    emit: false
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: "css-loader/locals"
+                    }
+                ]
+            },
+            {
+                test: /\.(eot|woff|woff2|ttf|svg|png|jpe?g|gif)(\?\S*)?$/,
+                loader: 'url-loader?limit=100000&name=./public/build/[hash].[ext]'
+            }
         ]
-      },
-      {
-        test: /js$/,
-        exclude: /(node_modules)/,
-        loader: "babel-loader",
-        query: { presets: ["react-app"] }
-      },
-        {
-            test: /\.(eot|woff|woff2|ttf|svg|png|jpe?g|gif)(\?\S*)?$/,
-            loader: 'url-loader?limit=100000&name=./public/build/[hash].[ext]'
-        }
+    },
+    plugins: [
     ]
-  }
 };
 
 module.exports = [browserConfig, serverConfig];
