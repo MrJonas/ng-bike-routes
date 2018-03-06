bad1526192d7a9094acaa7f8547838a92aeece0d
mrjonas <jonas.l.antanaitis@gmail.com>
2018-03-06 20:03:42 +0200

    test api url

--- src/shared/config.js
+++ src/shared/config.js
@@ -1,2 +1,2 @@
 export const getAppUrl = () => process.env.NODE_ENV === 'production' ?
-    'https://testas.dviraciumarsrutai.lt/' : 'http://localhost:3000/';
+    'https://test.dviraciumarsrutai.lt/' : 'http://localhost:3000/';
