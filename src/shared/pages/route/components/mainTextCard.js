1b1a04fe707e2099d836e65b19984ef684f431d9
mrjonas <jonas.l.antanaitis@gmail.com>
2018-03-20 20:21:35 +0200

    header nesting fixed

--- src/shared/pages/route/components/mainTextCard.js
+++ src/shared/pages/route/components/mainTextCard.js
@@ -4,7 +4,7 @@ import {Card, CardTitle, CardBlock} from 'reactstrap';
 const MainText = (props) => (
     <Card className="route-text">
         <CardBlock>
-            <CardTitle className="text-left"><h2>{props.route.title}</h2></CardTitle>
+            <h2 className="text-left">{props.route.title}</h2>
             <div dangerouslySetInnerHTML={{__html: props.route.body}}></div>
         </CardBlock>
     </Card>
