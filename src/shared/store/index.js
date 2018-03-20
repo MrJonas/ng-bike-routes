5b162067b73c400c06c595423ca3d99871c22f11
mrjonas <jonas.l.antanaitis@gmail.com>
2018-03-20 17:54:05 +0200

    Redux dev tool extention

--- src/shared/store/index.js
+++ src/shared/store/index.js
@@ -1,14 +1,17 @@
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import AppReducers from "./../reducers";
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-//TODO: Add redux dev tools
+const composeEnhancers = composeWithDevTools({
+    // options like actionSanitizer, stateSanitizer
+});
 
 const middleware = process.env.NODE_ENV !== 'production' ?
     [require('redux-immutable-state-invariant').default(), thunk] :
     [thunk];
 
 const configureStore = preloadedState =>
-    createStore(AppReducers, preloadedState, applyMiddleware(...middleware));
+    createStore(AppReducers, preloadedState, composeEnhancers(applyMiddleware(...middleware)));
 
 export default configureStore;
